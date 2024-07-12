25f2fe138fe883fff6b333194a511857e2c4eb5c
AnastasiyaKazachenka <anastasiya.kazachenka@mail.ru>
2024-07-12 11:46:10 +0100

    Add files via upload

---
+++ prepare_TPM_OS_table_TCGA_UVM.R
@@ -0,0 +1,26 @@
+setwd("~/ML_course/Lasso_Cox")
+
+expres <- TCGA_UVM[rowSums(TCGA_UVM[,c(3:82)]>5) > 20,]
+expres <- as.data.frame(expres)
+row.names(expres) <- expres$GeneID
+
+expres <- expres %>%
+  select(-GeneID,-GeneType) %>%
+  t() %>%
+  as.data.frame()
+
+row.names(expres) <- gsub("-01A","",row.names(expres))
+row.names(expres) <- gsub("-01B","",row.names(expres))
+
+row.names(clinical) <- clinical$submitter_id
+clinical$submitter_id <- NULL
+
+uvm <- merge(clinical,expres,by=0)
+uvm <- uvm %>%
+  mutate(OS = case_when(vital_status==1 ~ "False",
+                        vital_status==2 ~ "True"))
+
+row.names(uvm) <- uvm$Row.names
+uvm$Row.names <- NULL
+
+write.csv(uvm,file="TPMs_OS_TCGA_UVM.csv")
